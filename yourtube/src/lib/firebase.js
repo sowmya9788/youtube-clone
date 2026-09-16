460a45c4cd5f535bc95bbdc35f249de01b131b0a
sowmya9788 <sowmyanakka9788@gmail.com>
2026-09-16 19:34:03 +0530

    Fix Firebase Google sign in

--- yourtube/src/lib/firebase.js
+++ yourtube/src/lib/firebase.js
@@ -1,19 +1,10 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCmjNyU7GKm2hbR9GX1W5HHt0fMWGLpLPtvg",
-  authDomain: "youtube-70a86.firebaseapp.com",
-  projectId: "youtube-70a86",
-  storageBucket: "youtube-70a86.firebasestorage.app",
+  apiKey: "AIzaSyCnUjKUgM2hbR9GXIW5HHt0fMwGLpLPtvg",
+  authDomain: "yourtube-70a86.firebaseapp.com",
+  projectId: "yourtube-70a86",
+  storageBucket: "yourtube-70a86.firebasestorage.app",
   messagingSenderId: "21142295126",
   appId: "1:21142295126:web:82aed14e4bd646de96a1f7",
   measurementId: "G-87JK2LLZMP"
 };
-
-const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-
-export { auth, provider };
